3a9e5d25117b55763262133ef254f273ca6a9eb6
ccabreraf <camilo.cabrera@ymail.com>
2023-06-14 04:48:17 -0400

    arreglos menores

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 
 export const ItemListContainer = props => (
-    <Container>
+    <Container className='mt-4'>
         <h1>{props.greetings}</h1>
     </Container>
 )
