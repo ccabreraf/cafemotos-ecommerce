fb16da1de64082763545ef9967cdc888539f391e
ccabreraf <camilo.cabrera@ymail.com>
2023-07-06 18:30:28 -0400

    se crea json de productos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { ItemListContainer } from "./components/ItemListContainer";
 import { NavBar } from "./components/NavBar"
 
-function App() {
+import "./App.css"
+
+
+
+export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <NavBar />
-      <ItemListContainer greetings="Saludos!" />
-    </>
+      <Routes>
+        <Route path="/" element={<ItemListContainer greetings="Nuestros Productos" />} />
+        <Route path="/category/:id" element={<ItemListContainer greetings="Nuestros Productos" />} />
+      </Routes>
+      
+    </BrowserRouter>
   )
 }
-
-export default App;
