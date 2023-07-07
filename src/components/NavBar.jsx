fd6b3cecbf20b95a30701b687266ad37546e49b2
ccabreraf <camilo.cabrera@ymail.com>
2023-07-06 21:18:42 -0400

    arreglo navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,29 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import {NavLink} from "react-router-dom";
 import { CartWidget } from "./CartWidget";
+
+import data from "../data/products.json";
+
+const categorias = data.map(product => product.categoria)
+const unica = new Set(categorias)
+
+console.log([...unica])
 
 export const NavBar = () => (
     <Navbar bg="dark" variant="dark">
         <Container>
-            <Navbar.Brand href="#home">Cafe Motos</Navbar.Brand>
-            <Nav className="me-auto">
-                <Nav.Link href="#home">Inicio</Nav.Link>
-                <Nav.Link href="#features">Productos</Nav.Link>
-                <Nav.Link href="#pricing">Contacto</Nav.Link>
+            <Navbar.Brand>Cafe Motos</Navbar.Brand>
+            <Nav className="me-auto"> {[...unica].map(item => (
+                <NavLink key={item} className="nav-link" to={`/category/${item}`}>{item}</NavLink>
+            ))}
             </Nav>
             <CartWidget />
         </Container>
     </Navbar>
 )
 
+/* <Nav.Link href="#home">Inicio</Nav.Link>
+<Nav.Link href="#features">Productos</Nav.Link>
+<Nav.Link href="#pricing">Contacto</Nav.Link> */
