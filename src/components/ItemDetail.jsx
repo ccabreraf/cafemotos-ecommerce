be3a5f99f7acf3655ca11d3713c71a4a6a7f93cc
ccabreraf <camilo.cabrera@ymail.com>
2023-07-28 00:45:37 -0400

    arreglo ItemDetail

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -4,7 +4,7 @@ export const ItemDetail = ({ repuestos }) => {
             <div>{repuestos.id}</div>
             <div>{repuestos.nombre}</div>
             <div>{repuestos.categoria}</div>
-            <img src={repuestos.img} alt={repuestos.nombre} />
+            <img src={repuestos.img} alt={repuestos.nombre} height={'230vh'}/>
         </>
     )
 }
